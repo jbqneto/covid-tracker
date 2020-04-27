137bec479d48647b95db315ddb17f13daf5597fd
jbqneto <jbqneto@gmail.com>
2020-04-27 20:02:15 -0300

    changed the data recover responsible class

--- src/main/java/io/jbqneto/covidtracker/controller/HomeController.java
+++ src/main/java/io/jbqneto/covidtracker/controller/HomeController.java
@@ -1,15 +1,21 @@
 package io.jbqneto.covidtracker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.jbqneto.covidtracker.service.CovidService;
+
 @Controller
 public class HomeController {
 	
+	@Autowired
+	CovidService covidService;
+	
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("testName", "TESTE");
+		model.addAttribute("covidData", covidService.getData());
 		return "home";
 	}
 
